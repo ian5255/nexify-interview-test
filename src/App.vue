f09309cc412739066c66327c847badcc65e0e439
ian5258 <ian@axolotl.com.tw>
2022-09-17 19:06:34 +0800

    [add] init project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+<template lang="pug">
+  #app
+    transition(name="page" mode="out-in")
+      router-view
 </template>
 
 <style lang="scss">
